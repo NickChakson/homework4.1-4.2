59c4fc54fc6216ed29324f3bf2a0294ddd207cef
Gleb Gorbunov <gglebi01@gmail.com>
2022-04-24 16:37:23 +0500

    commit error

--- src/test/kotlin/HomeworkKtTest.kt
+++ src/test/kotlin/HomeworkKtTest.kt
@@ -8,7 +8,7 @@ class HomeworkKtTest {
     fun taxForTransferVisaMirWithFullFees() {
         val transfer: Double = 10_000_00.00
         val result: Int = taxForTransferVisaMir(_transfer = transfer).toInt()
-        assertEquals(7500, result)
+        assertEquals(75000, result)
 
     }
 
